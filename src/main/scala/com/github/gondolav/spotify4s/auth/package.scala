c1a7f92b19d94d7032db0c93bc359b998dbe1ce5
Andrea Veneziano <andrea.veneziano@epfl.ch>
2020-09-13 14:12:28 +0200

    Small fix to Scaladoc

--- src/main/scala/com/github/gondolav/spotify4s/auth/package.scala
+++ src/main/scala/com/github/gondolav/spotify4s/auth/package.scala
@@ -1,6 +1,6 @@
 package com.github.gondolav.spotify4s
 
 /**
- * This package contains classes useful for authentication.
+ * This package contains classes useful for [[https://developer.spotify.com/documentation/general/guides/authorization-guide/ authorization]].
  */
 package object auth
--- src/main/scala/com/github/gondolav/spotify4s/entities/package.scala
+++ src/main/scala/com/github/gondolav/spotify4s/entities/package.scala
@@ -1,7 +1,8 @@
 package com.github.gondolav.spotify4s
 
 /**
- * This package contains classes representing the objects obtainable by the API.
+ * This package contains classes representing the
+ * [[https://developer.spotify.com/documentation/web-api/reference/object-model/ objects]] obtainable by the API.
  *
  * Some endpoints return simplified versions of the resource objects. For this reason, the classes in this package
  * represent fields not present in the simplified versions as [[Option]]s.
